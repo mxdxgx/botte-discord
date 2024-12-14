47d720930340536809e96e1f46cd532731f904b2
madmax <=>
2024-12-14 18:44:57 -0500

    chore: add debug config, testing framework

--- src/client/discordClient.ts
+++ src/client/discordClient.ts
@@ -2,7 +2,14 @@ import { Client, GatewayIntentBits, Events } from 'discord.js';
 export class DiscordClient {
     client: Client;
     constructor(token: string, clientReadyHandler?: (client: Client) => void) {
-        this.client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+        this.client = new Client({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.MessageContent
+            ]
+        });
         this.client.login(token);
         this.client.once(Events.ClientReady, clientReadyHandler || (() => { }));
     }
