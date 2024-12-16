d201efb0e82449f6a12eeab0ed1f8250af794861
madmax <=>
2024-12-15 23:06:21 -0500

    test: experimenting with prisma

--- src/client/discordClient.ts
+++ src/client/discordClient.ts
@@ -1,5 +1,4 @@
-import { PrismaClient, welcome } from '@prisma/client';
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 export class DiscordClient {
     client: Client;
     constructor(token: string, clientReadyHandler?: (client: Client) => void) {
@@ -15,25 +14,5 @@ export class DiscordClient {
         });
         this.client.login(token);
         this.client.once(Events.ClientReady, clientReadyHandler || (() => { }));
-        // this.testDb().then(() => {
-        //     console.log("Tested DB");
-        // });
     }
-
-    // @TODO Remove this function
-    // This function is only for testing purposes
-    // It's there so i can remember how to use prisma
-    // when I am less tired and can think straight
-    // public async testDb() {
-    //     let prisma = await new PrismaClient();
-    //     const welcome = await prisma.welcome.create({
-    //         data: {
-    //             Field1: 2,
-    //             Field2: 2,
-    //             Field3: "",
-    //         },
-    //     });
-
-    //     console.log(welcome.Field1);
-    // }
 }
